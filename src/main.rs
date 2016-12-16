e1775b53e6c655c206d218b63262a4514f5196b9
Daniel Wiberg <daniel@skillster.se>
2016-12-16 15:45:05 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+
+extern crate gl;
+extern crate glutin;
+extern crate libc;
+
+
+fn main() {
+
+    let window = glutin::Window::new().unwrap();
+
+    unsafe { window.make_current(); }
+
+    unsafe {
+        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
+    }
+
+    for event in window.wait_events() {
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
+        window.swap_buffers();
+
+        match event {
+            glutin::Event::Closed => break,
+            _ => ()
+        }
+    }
+}
